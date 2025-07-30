573c7bbb83d05fb1743f9f76069dfda7fe786e2b
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-07-30 11:52:54 -0300

    add analise04

---
+++ analise04.py
@@ -0,0 +1,13 @@
+import main 
+
+##QUAIS SÃO AS 10 CIDADES COM O MAIOR TOTAL DE VENDAS?
+dados = main.df_dsa
+main.pd.set_option('display.max_columns', None)
+
+top10_cidades_vendas = dados.groupby('Cidade')['Valor_Venda'].sum().reset_index().sort_values(by= 'Valor_Venda', ascending= False)
+print(top10_cidades_vendas.head(10))
+
+main.plt.figure(figsize=(16,6))
+main.sns.set_palette('coolwarm')
+main.sns.barplot(data= top10_cidades_vendas.head(10),y='Valor_Venda', x='Cidade').set(title= 'As 10 Cidades com Maior Total de Vendas')
+main.plt.show()
